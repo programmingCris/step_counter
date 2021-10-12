5131eb34ae50fc4a73502e0277b411812476b0d9
krzysztofwolynski <k.praca55@gmail.com>
2021-10-12 16:01:42 +0200

    *dodanie atrybutow do widoku postępu

--- app/src/main/java/com/recrutation/fitsdktest/fit/view/FitCircularView.kt
+++ app/src/main/java/com/recrutation/fitsdktest/fit/view/FitCircularView.kt
@@ -12,8 +12,7 @@ import androidx.core.graphics.green
 import android.R.attr.radius
 
 import android.graphics.RectF
-
-
+import com.recrutation.fitsdktest.R
 
 
 class FitCircularView : View{
@@ -22,17 +21,42 @@ class FitCircularView : View{
     val trackPath = Path()
     private val progressPaint = Paint()
     private val trackPaint = Paint()
+    private var progress = 0
+    private var trackWidth = 10
 
     init {
-        progressPaint.color = Color.BLACK
-        trackPaint.color = Color.GREEN
         progressPaint.style = Paint.Style.STROKE
         trackPaint.style = Paint.Style.STROKE
+        progressPaint.isAntiAlias = true
+        trackPaint.isAntiAlias = true
+        progressPaint.strokeWidth = trackWidth.toFloat()
+        trackPaint.strokeWidth = trackWidth.toFloat()
     }
 
     constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs){
+        init(context, attrs)
+    }
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr){
+        init(context, attrs)
+    }
+
+    private fun init(context: Context, attrs: AttributeSet){
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.CircleProgress,
+            0, 0).apply {
+
+            try {
+                progressPaint.color = getColor(R.styleable.CircleProgress_custom_progressColor, Color.YELLOW)
+                trackPaint.color = getColor(R.styleable.CircleProgress_custom_trackColor, Color.GRAY)
+                progress = getInt(R.styleable.CircleProgress_custom_progress, 0)
+                trackWidth = getDimensionPixelSize(R.styleable.CircleProgress_custom_trackWidth, 0)
+            } finally {
+                recycle()
+            }
+        }
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
@@ -40,11 +64,12 @@ class FitCircularView : View{
     }
 
     private fun circleDraw(canvas: Canvas?){
-        val radius = (measuredWidth/2).toFloat()
+        val margin = measuredWidth/ 10
+        val radius = (measuredWidth/2).toFloat() - trackWidth - margin
         val mX = (measuredWidth/2).toFloat()
         val mY = (measuredHeight/2).toFloat()
         val oval = RectF(mX - radius, mY - radius, mX + radius, mY + radius)
+        canvas?.drawArc(oval, 0F, 360F, false, trackPaint)
         canvas?.drawArc(oval, -90F, 90F, false, progressPaint)
-        canvas?.drawArc(oval, -190F, 89F, false, trackPaint)
     }
 }
